9e4020c240ce1b0f7d2ca26fd5fa27c648f16508
jteo0 <jjtheodora@gmail.com>
2024-02-05 18:59:25 +0700

    fix position product test

---
+++ eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+public class ProductRepositoryTest {
+}
