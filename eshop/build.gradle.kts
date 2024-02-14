9db6e6618dac0236c4c95404fde5e9dd767877ed
jteo0 <jjtheodora@gmail.com>
2024-02-14 13:10:41 +0700

    build gradle kts edit

--- eshop/build.gradle.kts
+++ eshop/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
-
+/*
 sonar {
 	properties {
 		property("sonar.projectKey", "jteo0_tutorial-1")
@@ -12,7 +12,7 @@ sonar {
 		property("sonar.host.url", "https://sonarcloud.io")
 	}
 }
-
+*/
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
