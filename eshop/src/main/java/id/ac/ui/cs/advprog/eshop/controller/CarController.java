cf3c3b386083b5d750f051d129be0ba59edfae7e
jteo0 <jjtheodora@gmail.com>
2024-02-23 15:10:02 +0700

    seperating CarController and ProductController

--- eshop/src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,11 +1,10 @@
 package id.ac.ui.cs.advprog.eshop.controller;
-import id.ac.ui.cs.advprog.eshop.model.Car;;
+import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 
