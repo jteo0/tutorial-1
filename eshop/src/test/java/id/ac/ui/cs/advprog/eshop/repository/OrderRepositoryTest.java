a9d4af7de58e68712366245a14d64c99c5d4043b
jteo0 <jjtheodora@gmail.com>
2024-03-06 11:52:15 +0700

    [RED] Add tests for OrderServiceImpl

--- eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -20,7 +20,6 @@ class OrderRepositoryTest {
         Product product1 = new Product();
         product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cak Bambang");
-
         product1.setProductQuantity(2);
         products.add(product1);
 
